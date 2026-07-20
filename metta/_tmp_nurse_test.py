7b4f2377ce89361624f7bc66a7b1bb96c7ba2651
Sharmilaapf <sharmila.m@azimpremjifoundation.org>
2026-07-20 18:25:33 +0530

    first commit

---
+++ metta/_tmp_nurse_test.py
@@ -0,0 +1,31 @@
+
+import frappe
+
+
+def run():
+	pr = frappe.get_doc(
+		{
+			"doctype": "Patient Registration",
+			"registration_category": "OP",
+			"uhin_id": "HIN-000001",
+			"first_name": "placeholder",
+			"sex": "Female",
+		}
+	)
+	pr.insert(ignore_permissions=True)
+
+	ni = frappe.get_all(
+		"Nurse Interventions",
+		filters={"patient_registration": pr.name},
+		fields=["name", "status", "patient_unique_id", "patient_name"],
+	)
+
+	result = {"registration": pr.name, "nurse_interventions": ni}
+
+	# cleanup
+	for row in ni:
+		frappe.delete_doc("Nurse Interventions", row.name, force=True, ignore_permissions=True)
+	frappe.delete_doc("Patient Registration", pr.name, force=True, ignore
+				   _permissions=True)
+
+	return
